0d3546469a4253e1d4dd0fc5916cd2b8b4a2b640
devos10 <osbaldo081004@gmail.com>
2025-02-15 18:37:30 -0600

    Creamos la funcion agregar amigo, creamos un array amigos, validamos que no este vacio y limpiamos la caja de texto una vez que agregamos un nombre y damos click en añadir

--- app.js
+++ app.js
@@ -1,2 +1,21 @@
 // El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
 //comentario
+
+//creamos un array para almacenar nombres
+
+
+
+//creamos una funcion para agregar amigos 
+
+function agregarAmigo(){
+    let amigos= [];
+    let nombre =document.getElementById('amigo').value;
+    if(nombre==''){
+        alert("Por favor, inserte un nombre")
+    }else{
+        amigos.push(nombre);
+        console.log(amigos);
+        document.getElementById('amigo').value='';
+
+    }
+}
